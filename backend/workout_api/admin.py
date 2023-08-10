12c78b45c3477fa25ff199c05e7d6622f56c3216
Felix Mendez <mendez.felixrn@gmail.com>
2023-08-10 10:38:00 -0400

    change relation between workout component nad workout

--- backend/workout_api/admin.py
+++ backend/workout_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from  . import models
 # Register your models here.
+
+admin.site.register(models.BodyPart)
+admin.site.register(models.Exercise)
+admin.site.register(models.Pr)
+admin.site.register(models.WorkoutComponent)
+admin.site.register(models.Workout)
+admin.site.register(models.WorkoutRoutine)
